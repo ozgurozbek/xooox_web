8e49a5558eb3411677a45cffff9583cfcde26d61
Taha Çalık <70321288+tahacalik@users.noreply.github.com>
2021-11-23 15:02:10 +0300

    Add files via upload

---
+++ request.js
@@ -0,0 +1,21 @@
+const getBtn = document.getElementById('get-btn');
+
+
+const getData = () => {
+const xhr = new XMLHttpRequest()
+xhr.open('GET', 'http://xeculus.pythonanywhere.com/generate')
+
+xhr.responseType = "json"
+
+xhr.onload = () => {
+    const data = xhr.response
+    console.log(data)
+};
+
+xhr.send();
+
+}
+
+const sendData = () => {}
+
+getBtn.addEventListener('click', getData);
