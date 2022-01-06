b99ec17f10abbcde81b0f0398ae38b5e076e5dfe
Taha Çalık <tahacalik@outlook.com.tr>
2022-01-06 22:28:43 +0300

    Main Panel Format
    Main panel formatting
    Remove CSS
    Clean JS

--- static/js/script.js
+++ static/js/script.js
@@ -1,107 +1,17 @@
 const classX = 'x'
 const classO = 'o'
 const mainPanel = [
-  [0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0]
-]
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0,    
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0]
 
-const winMethot = [
-  [0, 1, 2],
-  [5, 6, 7],
-  [10, 11, 12],
-  [0, 5, 10],
-  [1, 6, 11],
-  [2, 7, 12],
-  [0, 6, 12],
-  [2, 6, 10],
-]
-const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
-const winMessageElement = document.getElementById('winMessage')
-const chooseElement = document.getElementById('choose')
-const restartButton = document.getElementById('restartButton')
 const result = document.getElementById('result')
-const xChoose = document.getElementById('xChoose')
-const oChoose = document.getElementById('oChoose')
 const loader = document.getElementById('timeout')
 
-const winMessageTextElement = document.querySelector('[data-winMsgText]')
-const chooseTextElement = document.querySelector('[data-chooseText]')
-var chosen = 0
-let turnO
 var xmlHttp = new XMLHttpRequest();
-
-//Bu bayağı kötü approach, düzeltmek lazım ileride, yoksa X seçmeden oyunu başlatacak.
-chosen = 1;
-startGame();
-console.log(chosen?"Game initiated as X":"Game initiated as O, something is wrong.");
-//-
-
-restartButton.addEventListener('click', startGame)
-
-function startGame() {
-  turnO = false
-  cellElements.forEach(cell => {
-    cell.classList.remove(classX)
-    cell.classList.remove(classO)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  winMessageElement.classList.remove('show')
-}
-
-function handleClick(e) {
-  const cell = e.target
-  const currentClass = classX
-  //var cellArray = Array.from(document.querySelectorAll('.cell')); //this code writes the cell name to the array. like div.cell.x div.cell.o or div.cell
-  placeMark(cell, currentClass)
-  if (checkWin(currentClass)) {
-    endGame(false)
-  } else if (isDraw()) {
-    endGame(true)
-  } else {
-    setBoardHoverClass()
-  }
-}
-
-function endGame(draw) {
-  if (draw) {
-    winMessageTextElement.innerText = 'Berabere!'
-  } else {
-    winMessageTextElement.innerText = `${turnO ? "O" : "X"} Kazandı!`
-  }
-  winMessageElement.classList.add('show')
-}
-
-function isDraw() {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(classX) || cell.classList.contains(classO)
-  })
-}
-
-function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
-}
-
-function setBoardHoverClass() {
-  board.classList.remove(classX)
-  board.classList.remove(classO)
-  if (turnO) {
-    board.classList.add(classO)
-  } else { board.classList.add(classX) }
-}
-
-function checkWin(currentClass) {
-  return winMethot.some(combination => {
-    return combination.every(index => {
-      return cellElements[index].classList.contains(currentClass)
-    })
-  })
-}
 
 function postButtonVal(theUrl) {
   xmlHttp.open("GET", theUrl, false); //Keep this false for discouraged synchronous request. Or don't.
@@ -111,11 +21,9 @@ function postButtonVal(theUrl) {
 
 function updateBoard(id) {
   loader.style.display = "flex";
-  const currentClass = classO;
-  const response = postButtonVal('/get?board=' + id);
-  const cellAi = document.getElementById(response)
-  cellAi.classList.add(currentClass)
-  //loader.style.display = "none";
+  mainPanel[id]= "x";
+  console.log(mainPanel)
+  // const response = postButtonVal('/get?board=' + id); 
 }
 
 
