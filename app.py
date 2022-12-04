398878e409df69420d51b3c481feebc6801254b6
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2022-12-04 03:10:09 +0000

    Display score calculation automatically

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ def root():
 @app.route('/get', methods=['GET']) # Maybe it can converted to a POST only, using a form to this.
 def get():
     input_board = [int(i) for i in request.args.get('board', default=empty_board, type=str).split(",")]
-    print(input_board)
     return jsonify(ai.runMinimax(input_board))
+
+@app.route('/get_result', methods=['GET']) # Maybe it can converted to a POST only, using a form to this.
+def get_result():
+    input_board = [int(i) for i in request.args.get('board', default=empty_board, type=str).split(",")]
+    return jsonify(ai.getResult(input_board))
 
 if __name__ == "__main__":
     app.run(threaded=True, host='0.0.0.0')
